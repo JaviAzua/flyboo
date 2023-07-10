81ed0d0c825d50fe37f57cb58509f92acd626c24
Javier Azua <azua.javi@gmail.com>
2023-07-10 15:14:36 -0300

    fixing dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Container from "@/components/Container";
 import EmptyState from "@/components/EmptyState";
 import getListings, { PropsParams } from "./actions/getListings";
